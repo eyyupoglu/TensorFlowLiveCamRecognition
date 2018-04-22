28356c22c6b773c957251beb908a95c858ade71a
mehmet <eyyupoglu.mehmet@gmail.com>
2018-04-22 02:33:13 +0200

    asdasd

--- pushPictureToGithub.py
+++ pushPictureToGithub.py
@@ -3,7 +3,7 @@ import subprocess
 def pushPictureToGithub(fileName):
     try:
         subprocess.call("(git pull)", shell=True)
-        subprocess.call("(git add *)", shell=True)
+        subprocess.call("(git add )" + fileName, shell=True)
         subprocess.call("(git commit -m \"asdasd\")", shell=True) 
         subprocess.call("(git push)", shell=True)
     except:
